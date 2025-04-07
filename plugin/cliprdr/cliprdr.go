f83b8fe42c6e7685bf0e2fe924ddbc4ab9fd14ac
trueHyper <volkoofsenya@gmail.com>
2025-04-07 17:22:10 +0300

    Update cliprdr.go

--- plugin/cliprdr/cliprdr.go
+++ plugin/cliprdr/cliprdr.go
@@ -9,9 +9,9 @@ import (
 
 	"github.com/lunixbochs/struc"
 
-	"github.com/tomatome/grdp/core"
-	"github.com/tomatome/grdp/glog"
-	"github.com/tomatome/grdp/plugin"
+	"github.com/trueHyper/rdp-scanner/core"
+	"github.com/trueHyper/rdp-scanner/glog"
+	"github.com/trueHyper/rdp-scanner/plugin"
 )
 
 /**
