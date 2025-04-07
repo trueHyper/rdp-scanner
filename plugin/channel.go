2b38e837f305870ef5e3f12d288fe33c4fcb0403
trueHyper <volkoofsenya@gmail.com>
2025-04-07 17:20:46 +0300

    Update channel.go

--- plugin/channel.go
+++ plugin/channel.go
@@ -6,10 +6,10 @@ import (
 	"fmt"
 	"unsafe"
 
-	"github.com/tomatome/grdp/glog"
+	"github.com/trueHyper/rdp-scanner/glog"
 
-	"github.com/tomatome/grdp/core"
-	"github.com/tomatome/grdp/emission"
+	"github.com/trueHyper/rdp-scanner/core"
+	"github.com/trueHyper/rdp-scanner/emission"
 )
 
 const (
