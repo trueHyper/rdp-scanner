e03cd361fe617f37511970681fea8ff56c9db533
trueHyper <volkoofsenya@gmail.com>
2025-04-07 17:15:31 +0300

    Update caps.go

--- protocol/pdu/caps.go
+++ protocol/pdu/caps.go
@@ -7,11 +7,11 @@ import (
 	"fmt"
 	"io"
 
-	"github.com/tomatome/grdp/glog"
+	"github.com/trueHyper/rdp-scanner/glog"
 
 	"github.com/lunixbochs/struc"
-	"github.com/tomatome/grdp/core"
-	"github.com/tomatome/grdp/protocol/t125/gcc"
+	"github.com/trueHyper/rdp-scanner/core"
+	"github.com/trueHyper/rdp-scanner/protocol/t125/gcc"
 )
 
 type CapsType uint16
