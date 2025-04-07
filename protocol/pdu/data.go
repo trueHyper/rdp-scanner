dee4aadd2c28631431d9eb4049727e32e44ed5c0
trueHyper <volkoofsenya@gmail.com>
2025-04-07 17:15:57 +0300

    Update data.go

--- protocol/pdu/data.go
+++ protocol/pdu/data.go
@@ -7,8 +7,8 @@ import (
 	"io"
 
 	"github.com/lunixbochs/struc"
-	"github.com/tomatome/grdp/core"
-	"github.com/tomatome/grdp/glog"
+	"github.com/trueHyper/rdp-scanner/core"
+	"github.com/trueHyper/rdp-scanner/glog"
 )
 
 const (
